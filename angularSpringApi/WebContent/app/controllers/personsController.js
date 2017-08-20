9fa6189322cdf28987b1e780b4e19e754823d6b9
ArslanAnjum <muhammad.arslan.anjum@gmail.com>
2017-08-20 22:50:27 +0500

    decoupling user configuration.

--- angularSpringApi/WebContent/app/controllers/personsController.js
+++ angularSpringApi/WebContent/app/controllers/personsController.js
@@ -12,36 +12,43 @@ app.controller('personsController',
 				$scope.csrfToken = csrfToken;
 				$scope.csrfHeaderName = csrfHeaderName;
 				$scope.server = server;
-				
 				var dataTable = entity + 'DataTable';
 				var createForm = entity + 'Create';
+				
+				
+				var dataTableMetadata = 
+					{
+						name 		:{iType:'input',required:true,editable:true,searchable:true,inGridVisible:true},
+						address		:{iType:'input',required:true,editable:true,searchable:true,inGridVisible:true},
+						emailId 	:{iType:'input',required:true,editable:true,searchable:true,inGridVisible:true},
+						phoneNumber :{iType:'input',required:true,editable:true,searchable:true,inGridVisible:true},
+						city		:{iType:'searchable-dropdown',fetch:'cities',required:true,editable:true,searchable:true,inGridVisible:true},
+						industry	:{iType:'searchable-dropdown',fetch:'industries',required:true,editable:true,searchable:true,inGridVisible:true},
+						badges		:{iType:'multiselect-dropdown',fetch:'badges',editable:true,searchable:true,inGridVisible:true}
+					};
+				
+				var createFormMetadata = 
+					{
+						name 		:{iType:'input',required:true},
+						address		:{iType:'input',required:true},
+						emailId 	:{iType:'input',type:'email',required:true},
+						phoneNumber :{iType:'input',required:true},
+						city		:{iType:'searchable-dropdown',fetch:'cities',required:true},
+						industry	:{iType:'searchable-dropdown',fetch:'industries',required:true},
+						badges		:{iType:'multiselect-dropdown',fetch:'badges'}
+					};
+				
+				
 				
 				$scope[dataTable] = new apiDataTable(
 						$scope,
 						entity,
-						{
-							name 		:{iType:'input',required:true,editable:true,searchable:true,inGridVisible:true},
-							address		:{iType:'input',required:true,editable:true,searchable:true,inGridVisible:true},
-							emailId 	:{iType:'input',required:true,editable:true,searchable:true,inGridVisible:true},
-							phoneNumber :{iType:'input',required:true,editable:true,searchable:true,inGridVisible:true},
-							city		:{iType:'searchable-dropdown',fetch:'cities',required:true,editable:true,searchable:true,inGridVisible:true},
-							industry	:{iType:'searchable-dropdown',fetch:'industries',required:true,editable:true,searchable:true,inGridVisible:true},
-							badges		:{iType:'multiselect-dropdown',fetch:'badges',editable:true,searchable:true,inGridVisible:true}
-						}
+						dataTableMetadata
 				);
-				
 				$scope[createForm] = new apiForm(
 						$scope,
 						entity,
-						{
-							name 		:{iType:'input',required:true},
-							address		:{iType:'input',required:true},
-							emailId 	:{iType:'input',type:'email',required:true},
-							phoneNumber :{iType:'input',required:true},
-							city		:{iType:'searchable-dropdown',fetch:'cities',required:true},
-							industry	:{iType:'searchable-dropdown',fetch:'industries',required:true},
-							badges		:{iType:'multiselect-dropdown',fetch:'badges'}
-						},
+						createFormMetadata,
 						function(){
 							$scope[dataTable].update();
 						}
