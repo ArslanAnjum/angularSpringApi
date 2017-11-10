fe99b93986ddbe1a6155ace6c0045410830cb36d
ArslanAnjum <muhammad.arslan.anjum@gmail.com>
2017-11-10 18:11:25 +0500

    updated with spring boot.

--- angularSpringApi/src/com/arslan/angularSpringApi/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.arslan.angularSpringApi.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
-
-}
