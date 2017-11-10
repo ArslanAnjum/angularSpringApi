99ec0078515cb0371bdfdb43971e580c05bd1e91
ArslanAnjum <muhammad.arslan.anjum@gmail.com>
2017-11-10 18:50:47 +0500

    directory structure changed to deploy on heorku

--- angularSpringApi/sqlScripts/badge.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "badge" VALUES (1, 'Gold');
-INSERT INTO "badge" VALUES (2, 'Silver');
-INSERT INTO "badge" VALUES (3, 'Bronze');
-INSERT INTO "badge" VALUES (4, 'Curious');
-INSERT INTO "badge" VALUES (5, 'Affector');
